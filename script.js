fba274ac54bbe996d57e15adc4ecccd87c3105f3
PhuVu <phuducvu99@gmail.com>
2020-12-01 20:07:21 +0700

    Finished Infinity Scroll

--- script.js
+++ script.js
@@ -2,12 +2,27 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 //Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'D-DR_MZ4MNEITpmvCynAcVrmaX22xC-l94Dyb31w3e4';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//Check if all images were loaded
+function imageLoaded(){
+    // console.log('image loaded');
+    imagesLoaded++;
+    console.log(imagesLoaded)
+    if(imagesLoaded === totalImages){
+        ready = true;
+        loader.hidden = true;
+        console.log('ready =', ready);
+    }
+}
 
 //Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes){
@@ -18,6 +33,9 @@ function setAttributes(element, attributes){
 
 //Create Elements For Links and Photos, Add to DOM
 function displayPhotos(){
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log('total images', totalImages);
     //Run function for each object in photosArray
     photosArray.forEach((photo) => {
         //Create <a> to link to Unsplash
@@ -40,6 +58,9 @@ function displayPhotos(){
             title: photo.alt_description,
         });
 
+        //Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
+
         //Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -57,6 +78,15 @@ async function getPhotos(){
         console.log()
     }
 }
+
+//Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+    // console.log('scrolled');
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
+        getPhotos();
+    }
+})
 
 //On load
 getPhotos();
